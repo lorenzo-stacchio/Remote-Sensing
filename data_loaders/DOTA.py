08b9f314c2a37c30c49a82eaf821144ac99c888b
lorenzo-stacchio <lorenzo.stacchio.dev@gmail.com>
2024-10-16 18:51:19 +0000

    to fix dataloader

--- data_loaders/DOTA.py
+++ data_loaders/DOTA.py
@@ -17,13 +17,19 @@ from feature_extractor import Model_type
 import itertools
 import copy
 import tqdm 
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
 
-# DATASET FROM
-# https://huggingface.co/datasets/timm/resisc45
-# https://www.tensorflow.org/datasets/catalog/resisc45?hl=it
-# SPLITS FROM
-# https://github.com/google-research/google-research/blob/master/remote_sensing_representations/README.md#dataset-splits
+## MANAGE BBOX AUGMENTATIONS
 
+## https://pytorch.org/vision/stable/auto_examples/transforms/plot_transforms_e2e.html#sphx-glr-auto-examples-transforms-plot-transforms-e2e-py
+
+## https://chatgpt.com/share/6710089f-59ac-8008-93b6-bd8b2a521eaf
+
+
+
+# OBJECT DETECTION
+## https://pytorch.org/tutorials/intermediate/torchvision_tutorial.html
 
 class DOTAv1(Dataset):
 
@@ -32,7 +38,6 @@ class DOTAv1(Dataset):
 
     
     def denormalize(self, image):
-        # mean, std = (0.485, 0.456, 0.406), (0.229, 0.224, 0.225)
         mean = torch.tensor(self.mean).view(1, 3, 1, 1)
         std = torch.tensor(self.std).view(1, 3, 1, 1)
 
@@ -47,15 +52,13 @@ class DOTAv1(Dataset):
             
         
     def get_split(self,dataset_path, partition):     
-        ## dataframe path
-        
-           
+        ## dataframe path          
         image_dir = dataset_path + f"{partition}/images/"
         # dataset/DOTA/FIXED/train/labelTxt-v1.0/Train_Task2_gt/trainset_reclabelTxt
         files = [txt_file for txt_file in glob.glob(dataset_path + f"{partition}/labelTxt-v1.0/{partition.capitalize()}_Task2_gt/{partition}set_reclabelTxt/*.txt")]    
         # print(files)
         columns = ["image_path", "partition"]
-        coordinates = [item for sublist in [[f"x_{i}", f"j_{i}"] for i, j in zip(range(4), range(4))] for item in sublist]
+        coordinates = [item for sublist in [[f"x_{i}", f"y_{i}"] for i, j in zip(range(4), range(4))] for item in sublist]
         ann_coordinates = copy.deepcopy(coordinates)
         ann_coordinates.extend(["class","hard_object"])
         
@@ -64,27 +67,34 @@ class DOTAv1(Dataset):
         df = pd.DataFrame(columns=columns)
         
         # all images
-        # image_files =  [img_file for img_file in glob.glob(f"{image_dir}/*.png")]   
+        image_files =  [img_file for img_file in glob.glob(f"{image_dir}/*.png")]   
+        assert len(image_files) == len(files), print("DIFFERENT LENGTH", len(image_files), len(files))
+
         # print(len(image_files), len(files)) 
         dict_lists_dataframe = {"image_path": [], "partition": []}
         dict_lists_dataframe.update({index:[] for index in ann_coordinates})
         print(dict_lists_dataframe.keys())
-        # exit()
-        
+
         for file_ann in tqdm.tqdm(files, total = len(files), desc = f"Building {partition} dataset"):
             # print(file_ann)
             image_path = f"{image_dir}{os.path.basename(file_ann).split('.')[0]}.png"
             # print(image_path)
-            # assert os.path.exists(image_path)
+            assert os.path.exists(image_path)
             objects_ann = [line.replace("\n", "") for line in open(file_ann).readlines()]
             for obj_ann in objects_ann:
                 dict_lists_dataframe["image_path"].append(image_path)
                 dict_lists_dataframe["partition"].append(partition)
+                ## coordinates
                 for index, value in zip(ann_coordinates, obj_ann.split(" ")): 
                     dict_lists_dataframe[index].append(value)
         
         for col in df.columns:
             df[col] = dict_lists_dataframe[col]
+        
+        # float to int for 
+        for c in coordinates:
+            df[c] = df[c].astype(float)
+
         # print(len(df))
         # print(df.head(5))                
         return df 
@@ -96,6 +106,8 @@ class DOTAv1(Dataset):
         # filter partition
         self.partition = split
         self.dataframe_csv = self.get_split(dataset_path, self.partition)
+        
+        self.size = 512
         # print(list(self.dataframe_csv["image_path"][:10]))
         # print(self.dataframe_csv.head(10))
         
@@ -107,8 +119,8 @@ class DOTAv1(Dataset):
     def train_transform(self):
         return transforms.Compose([
             self._convert_image_to_rgb,
-            transforms.Resize(256, interpolation=InterpolationMode.BICUBIC),
-            transforms.RandomCrop(224),
+            transforms.Resize((self.size,self.size) interpolation=InterpolationMode.BICUBIC),
+            # transforms.RandomCrop(self.size),
             transforms.ToTensor(),
             # Normalize with mean and std of imagenet
             transforms.Normalize(self.mean,self.std)
@@ -117,7 +129,7 @@ class DOTAv1(Dataset):
     def test_transform(self):
         return transforms.Compose([
         self._convert_image_to_rgb,
-        transforms.Resize((224,224), interpolation=InterpolationMode.BICUBIC),
+        transforms.Resize((self.size,self.size), interpolation=InterpolationMode.BICUBIC),
         # transforms.CenterCrop(224),
         transforms.ToTensor(),
         # Normalize with mean and std of imagenet
@@ -145,6 +157,10 @@ class DOTAv1(Dataset):
             print("\n\n NAME", name)
 
         data = {'image': image.to(self.device), "image_path":name, 'class': self.make_label(label)}
+        
+        ## TODO: sta roba come tensore 
+        coordinates = [item for sublist in [[f"x_{i}", f"y_{i}"] for i, j in zip(range(4), range(4))] for item in sublist]
+        data.update({coord:row_index[coord] for coord in coordinates})
 
         return data
 
@@ -179,14 +195,24 @@ if __name__ == "__main__":
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     
-    # for i, data in enumerate(train_dataloader):
-    #     print(i, data["image_path"], data["class"])
+    for i, data in enumerate(train_dataloader):
+        print(data.keys())
+        print(i, data["image_path"], data["class"])
+        x_0 = int(data['x_0'])
+        y_0 = int(data['y_0'])
+        x_1 = int(data['x_1'])
+        y_1 = int(data['y_1'])
+        x_2 = int(data['x_2'])
+        y_2 = int(data['y_2'])
+        x_3 = int(data['x_3'])
+        y_3 = int(data['y_3'])        
+        print(x_0, y_0, x_2, y_2) # anti
+        image_tensor = data["image"]
+        image_tensor = train_dataset.denormalize(image_tensor)
+        image_tensor = image_tensor[0]
+        image_pil = transforms.ToPILImage()(image_tensor)
+        # image_pil = image_pil.crop((x_0, y_0, x_2, y_2))
 
-    #     image_tensor = data["image"]
-    #     image_tensor = train_dataset.denormalize(image_tensor)
-    #     image_tensor = image_tensor[0]
-    #     to_pil = transforms.ToPILImage()
-    #     image_pil = to_pil(image_tensor)
-    #     image_pil.save(f'{out_dir}/image{i}.jpg')
-    #     if i > 10:
-    #         break
+        image_pil.save(f'{out_dir}/image{i}.jpg')
+        if i > 10:
+            break
